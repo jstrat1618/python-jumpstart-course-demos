7c6a665bfe1110cada029c6f8910479e427a1d63
Justin <jstrat1.618@gmail.com>
2018-02-11 22:04:23 -0700

    Add actors to game.

--- apps/07_wizard_battle/you_try/my_app/Notes.md
+++ apps/07_wizard_battle/you_try/my_app/Notes.md
@@ -1,2 +1,16 @@
 # Wizard App
 Here, we will have a game where a wizard battles several creatures.
+
+##### Note: In this app we do a lot of management of both the game and application. Ideally, in a real application, we should separate those two functions.
+ 
+## Classes
+In python classes are defined with the keyword class. 
+__Conventionally, classes in python are defined using upper cases! (e.g BeautifulSoup), most other objects are defined using lower case.__
+
+### Magic Methods
+Magic methods are methods that apply to a particular class. They are defined inside the class body with the def function with the __ preceeding and succeeding the name of the magic method.
+The most import magic method is \_\_init__, which initializes the object being created. The parameter self is required in the \_\_init__ method. This points to the object inside the class.
+All methods inside a class take the self parameter.
+
+The \_\_repr__ defines a representation of an object. This method is called by print. So you can define how your objects print.
+
--- apps/07_wizard_battle/you_try/my_app/program.py
+++ apps/07_wizard_battle/you_try/my_app/program.py
@@ -1,3 +1,9 @@
+import random
+
+import time
+
+from actors import Wizard, Creature
+
 def main():
     #print header
     print_header()
@@ -11,14 +17,37 @@ def print_header():
     print("------------------------------------------")
 
 def game_loop():
+    creatues = [Creature("Toad", 1),
+                Creature("Tiger",12 ),
+                Creature("Bat",3),
+                Creature("Dragon", 50),
+                Creature("Evil Wizard", 1000)]
+
+
+    hero = Wizard("Gandolf", 75)
+
     while True:
+        active_creature = random.choice(creatues)
+
+        print("A {} of level {} has appeared from a dark and foggy forest...."
+              .format(active_creature.name, active_creature.level))
+
         cmd = input("Do you [a]ttack, [r]un away, [l]ook around or [e]xit? ")
-        if cmd.strip().lower() == 'a':
-            print('a')
+        cmd = cmd.strip().lower()
+        if cmd == 'a':
+            if hero.attack(active_creature):
+                creatues.remove(active_creature)
+            else:
+                print("The wizard {} hides to recover".format(hero.name))
+                time.sleep(3)
+                print("The wizard {} returns revitalized!".format(hero.name))
         elif cmd == 'r':
-            print('r')
+            print('The wizard is unsure of his power and runs away.')
         elif cmd == 'l':
-            print("l")
+            print("The wizard {} looks around and sees: ".format(hero.name))
+            for c in creatues:
+                print("A {} of level {}".format(c.name, c.level))
+
         elif cmd == 'e':
             break
         else:
